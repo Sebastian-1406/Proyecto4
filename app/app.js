6a6cc90116494fea480c82b226e3353a2e3846ab
Sebastian1406 <SCsebastian1406@gmail.com>
2021-07-10 18:25:07 -0500

    prueba

---
+++ app/app.js
@@ -0,0 +1,9 @@
+let boton = document.getElementById("boton")
+let contenedor = document.querySelector("body")
+
+
+boton.addEventListener("click", () => {
+
+	contenedor.classList.toggle("active")
+
+})
